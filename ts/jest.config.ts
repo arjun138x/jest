307bbbf655609d8f2bc4ee49f4221d8d3e8169c2
techmalli333 <techmalli333@gmail.com>
2024-04-27 12:36:57 +0530

    debugging

--- ts/jest.config.ts
+++ ts/jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "node", // this helps for debugging the jest
   verbose: true,
 };
 
