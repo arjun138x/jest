6b3925a4daa775ab85195bb48fedb549fa2d81d0
techmalli333 <techmalli333@gmail.com>
2024-04-27 13:02:38 +0530

    code coverage

--- ts/jest.config.ts
+++ ts/jest.config.ts
@@ -3,6 +3,8 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node", // this helps for debugging the jest
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
 };
 
 export default config;
